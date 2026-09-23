d8d15195cac167f87130ae8be81dd7f52bc23b85
Zetty <azzaizzaty0306@gmail.com>
2026-09-23 14:37:05 +0800

    Set up project structure and product model

---
+++ lib/data/models/product.dart
@@ -0,0 +1,33 @@
+class Product {
+  final int id;
+  final String title;
+  final String description;
+  final double price;
+  final double rating;
+  final String thumbnail;
+  final List<String> images;
+
+  const Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.rating,
+    required this.thumbnail,
+    required this.images,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'] as int,
+      title: json['title'] as String? ?? '',
+      description: json['description'] as String? ?? '',
+      price: (json['price'] as num?)?.toDouble() ?? 0,
+      rating: (json['rating'] as num?)?.toDouble() ?? 0,
+      thumbnail: json['thumbnail'] as String? ?? '',
+      images: (json['images'] as List<dynamic>? ?? [])
+          .map((image) => image.toString())
+          .toList(),
+    );
+  }
+}
