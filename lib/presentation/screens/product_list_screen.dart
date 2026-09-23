036c81ffe255908263440a95dd37b1e294726767
Zetty <azzaizzaty0306@gmail.com>
2026-09-23 15:48:44 +0800

    Connect product list to product data

--- lib/presentation/screens/product_list_screen.dart
+++ lib/presentation/screens/product_list_screen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../controllers/product_controller.dart';
+import '../widgets/product_card.dart';
 
 class ProductListScreen extends StatelessWidget {
   const ProductListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = context.watch<ProductController>();
+
     return Scaffold(
       appBar: AppBar(title: const Text('Product Catalog')),
-      body: const Center(child: Text('Products will appear here')),
+      body: controller.isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: controller.products.length,
+              itemBuilder: (context, index) {
+                final product = controller.products[index];
+
+                return ProductCard(product: product);
+              },
+            ),
     );
   }
 }
