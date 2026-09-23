36a17ea8bf09d6642d94440ea127f9cf9e2815a1
Zetty <azzaizzaty0306@gmail.com>
2026-09-23 16:15:33 +0800

    Add infinite scroll pagination to product list

--- lib/presentation/screens/product_list_screen.dart
+++ lib/presentation/screens/product_list_screen.dart
@@ -5,8 +5,40 @@ import '../controllers/product_controller.dart';
 import '../widgets/product_card.dart';
 import 'product_detail_screen.dart';
 
-class ProductListScreen extends StatelessWidget {
+class ProductListScreen extends StatefulWidget {
   const ProductListScreen({super.key});
+
+  @override
+  State<ProductListScreen> createState() => _ProductListScreenState();
+}
+
+class _ProductListScreenState extends State<ProductListScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController.addListener(_onScroll);
+  }
+
+  void _onScroll() {
+    if (!_scrollController.hasClients) {
+      return;
+    }
+
+    final position = _scrollController.position;
+
+    if (position.pixels >= position.maxScrollExtent - 300) {
+      context.read<ProductController>().loadMore();
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,23 +46,37 @@ class ProductListScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: const Text('Product Catalog')),
-      body: controller.isLoading
+      body: controller.isLoading && controller.products.isEmpty
           ? const Center(child: CircularProgressIndicator())
           : ListView.builder(
-              itemCount: controller.products.length,
+              controller: _scrollController,
+              padding: const EdgeInsets.all(16),
+              itemCount:
+                  controller.products.length +
+                  (controller.isLoadingMore ? 1 : 0),
               itemBuilder: (context, index) {
+                if (index >= controller.products.length) {
+                  return const Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Center(child: CircularProgressIndicator()),
+                  );
+                }
+
                 final product = controller.products[index];
 
-                return ProductCard(
-                  product: product,
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (_) =>
-                            ProductDetailScreen(productId: product.id),
-                      ),
-                    );
-                  },
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 12),
+                  child: ProductCard(
+                    product: product,
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (_) =>
+                              ProductDetailScreen(productId: product.id),
+                        ),
+                      );
+                    },
+                  ),
                 );
               },
             ),
